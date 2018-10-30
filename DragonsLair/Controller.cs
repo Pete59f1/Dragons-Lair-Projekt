542b97a476574f742b2a179bd058a7b2141ee714
Pete59f1 <pete59f1@edu.eal.dk>
2018-10-30 10:33:09 +0100

    Prøvede at få showscore til at virke igen, men nej.....

--- DragonsLair/Controller.cs
+++ DragonsLair/Controller.cs
@@ -13,23 +13,22 @@ namespace DragonsLair
         {
             Tournament t;
             t = tournamentRepository.GetTournament(tournamentName);
-            Round currentRound;
             List<Team> winningTeams = new List<Team>();
-            int rounds = t.GetNumberOfRounds();
-            int[] scores = new int[t.GetTeams().ToArray().Length];
+            Team[] teams = t.GetTeams().ToArray();
+            int[] scores = new int[teams.Length];
 
-            for(int i = 0; i < rounds; i++)
+            for(int i = 0; i < t.GetNumberOfRounds(); i++)
             {
-                currentRound = t.GetRound(i);
+                Round currentRound = t.GetRound(i);
                 winningTeams = currentRound.GetWinningTeams();
 
-                for(int team = 0; team < t.GetTeams().ToArray().Length; team++)
+                for(int j = 0; j < teams.Length; j++)
                 {
                     for(int winningTeam = 0; winningTeam < winningTeams.Count; winningTeam++)
                     {
-                        if(t.GetTeams().ToArray()[team].Name == winningTeams[winningTeam].Name)
+                        if(teams[j].Name == winningTeams[winningTeam].Name)
                         {
-                            scores[team]++;
+                            scores[j]++;
                         }
                     }
                 }
@@ -37,11 +36,11 @@ namespace DragonsLair
 
             for(int i = scores.Max(); i >= 0; i--)
             {
-                for(int j = 0; j < t.GetTeams().ToArray().Length; j++)
+                for(int j = 0; j < teams.Length; j++)
                 {
                     if(scores[j] == i)
                     {
-                        Console.WriteLine("Team: " + t.GetTeams().ToArray()[j] + " - Score: " + scores[j]);
+                        Console.WriteLine("Team: " + teams[j] + " - Score: " + scores[j]);
                     }
                 }
             }
