25a26dbfd60554314d25776258e9b2b7abc8046a
Peter Starup <pete59f1@edu.eal.dk>
2018-11-16 12:30:11 +0100

    NY

--- TournamentLibrary/TournamentRepo.cs
+++ TournamentLibrary/TournamentRepo.cs
@@ -4,8 +4,6 @@ namespace TournamentLib
 {
     public class TournamentRepo
     {
-        private Tournament winterTournament = new Tournament("Vinter Turnering");
-
         public Tournament GetTournament(string name)
         {
             StreamReader txtReader = new StreamReader("Gem turneringer.txt");
@@ -15,17 +13,9 @@ namespace TournamentLib
             {
                 if (line.ToLower() == name.ToLower())
                 {
-                    if (line == "vinter turnering")
-                    {
-                        txtReader.Close();
-                        return winterTournament;
-                    }
-                    else
-                    {
-                        Tournament tournament = new Tournament(name);
-                        txtReader.Close();
-                        return tournament;
-                    }
+                    Tournament tournament = new Tournament(name);
+                    txtReader.Close();
+                    return tournament;
                 }
             }
             txtReader.Close();
